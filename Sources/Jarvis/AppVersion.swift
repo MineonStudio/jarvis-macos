9a04e18df4d5b606ee0daedf7b535fcbb9d4a839
wesley <wesley.miao@outlook.com>
2026-08-16 23:17:36 +0800

    fix: restore rounded dock icon

--- Sources/Jarvis/AppVersion.swift
+++ Sources/Jarvis/AppVersion.swift
@@ -7,8 +7,8 @@ enum JarvisAppVersion {
     static let releasesURL = URL(string: "https://github.com/MineonStudio/jarvis-macos/releases")
         ?? URL(fileURLWithPath: "/")
 
-    private static let fallbackShortVersion = "0.5.69"
-    private static let fallbackBuild = "143"
+    private static let fallbackShortVersion = "0.5.70"
+    private static let fallbackBuild = "144"
 
     static var shortVersion: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String

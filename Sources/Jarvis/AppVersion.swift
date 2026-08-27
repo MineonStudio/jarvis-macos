d04e70f921f363005eb2ea3d05404f882ee98e0a
wesley <wesley.miao@outlook.com>
2026-08-27 18:35:21 +0800

    chore: bump release version to 1.0

--- Sources/Jarvis/AppVersion.swift
+++ Sources/Jarvis/AppVersion.swift
@@ -7,8 +7,8 @@ enum JarvisAppVersion {
     static let releasesURL = URL(string: "https://github.com/MineonStudio/jarvis-macos/releases")
         ?? URL(fileURLWithPath: "/")
 
-    private static let fallbackShortVersion = "0.9.9"
-    private static let fallbackBuild = "223"
+    private static let fallbackShortVersion = "1.0"
+    private static let fallbackBuild = "224"
 
     static var shortVersion: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String

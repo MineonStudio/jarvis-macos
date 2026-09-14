ea4e709d5ccab7e84feaa4ae8d068813f0deeb3c
wesley <wesley.miao@outlook.com>
2026-09-14 21:28:55 +0800

    release: prepare Jarvis 1.2.25

--- Sources/Jarvis/AppVersion.swift
+++ Sources/Jarvis/AppVersion.swift
@@ -8,8 +8,8 @@ enum JarvisAppVersion {
     static let releasesURL = URL(string: "https://github.com/MineonStudio/jarvis-macos/releases")
         ?? URL(fileURLWithPath: "/")
 
-    private static let fallbackShortVersion = "1.2.24"
-    private static let fallbackBuild = "258"
+    private static let fallbackShortVersion = "1.2.25"
+    private static let fallbackBuild = "259"
 
     static var shortVersion: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String

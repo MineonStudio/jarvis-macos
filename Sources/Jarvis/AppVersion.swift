4a0b084185f41e8f1baf4e4ea0fdf3c773d769bd
wesley <wesley.miao@outlook.com>
2026-09-24 22:45:43 +0800

    release: prepare Jarvis 1.4.3

--- Sources/Jarvis/AppVersion.swift
+++ Sources/Jarvis/AppVersion.swift
@@ -5,8 +5,8 @@ import Foundation
 import Security
 
 enum JarvisAppVersion {
-    private static let fallbackShortVersion = "1.4.2"
-    private static let fallbackBuild = "340"
+    private static let fallbackShortVersion = "1.4.3"
+    private static let fallbackBuild = "341"
 
     static var shortVersion: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
